bc970941f19d1dfd54e849d2bbaa98f3f82b617e
Christoph Hösler <christoph.hoesler@gmail.com>
2014-08-15 11:41:36 +0200

    Switched to migrations

--- application/migrations/20140815111600_create_base.php
+++ application/migrations/20140815111600_create_base.php
@@ -1,27 +1,13 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: bzdinf1
--- ------------------------------------------------------
--- Server version	5.0.67
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+class Migration_create_base extends CI_Migration {
 
---
--- Table structure for table `ci_sessions`
---
+	public function up() {
 
-DROP TABLE IF EXISTS `ci_sessions`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+    $this->db->query('SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0');
+
+		## Create Table ci_sessions
+		$this->db->query(<<<'EOT'
 CREATE TABLE `ci_sessions` (
   `session_id` varchar(40) collate utf8_bin NOT NULL default '0',
   `ip_address` varchar(16) collate utf8_bin NOT NULL default '0',
@@ -29,16 +15,12 @@ CREATE TABLE `ci_sessions` (
   `last_activity` int(10) unsigned NOT NULL default '0',
   `user_data` text collate utf8_bin NOT NULL,
   PRIMARY KEY  (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `contributions`
---
-
-DROP TABLE IF EXISTS `contributions`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+EOT
+);
+		
+    ## Create Table contributions
+		$this->db->query(<<<'EOT'
 CREATE TABLE `contributions` (
   `id` int(11) NOT NULL auto_increment,
   `participantId` int(11) NOT NULL,
@@ -59,30 +41,22 @@ CREATE TABLE `contributions` (
   KEY `symposiumId` (`symposiumId`),
   CONSTRAINT `contributions_ibfk_2` FOREIGN KEY (`symposiumId`) REFERENCES `symposia` (`id`),
   CONSTRAINT `contributions_ibfk_3` FOREIGN KEY (`participantId`) REFERENCES `participants` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1091 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=1091 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `groups`
---
-
-DROP TABLE IF EXISTS `groups`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table groups
+		$this->db->query(<<<'EOT'
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(80) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `lhc_slides`
---
-
-DROP TABLE IF EXISTS `lhc_slides`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table lhc_log
+		$this->db->query(<<<'EOT'
 CREATE TABLE `lhc_slides` (
   `id` int(11) NOT NULL auto_increment,
   `content` text collate utf8_unicode_ci NOT NULL,
@@ -91,60 +65,44 @@ CREATE TABLE `lhc_slides` (
   `endTime` time NOT NULL,
   `days` varchar(255) collate utf8_unicode_ci NOT NULL default '[]',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=63 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=63 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `lhc_log`
---
-
-DROP TABLE IF EXISTS `lhc_log`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table lhc_slides
+		$this->db->query(<<<'EOT'
 CREATE TABLE `lhc_log` (
   `ts` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `message` text collate utf8_unicode_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `login_attempts`
---
-
-DROP TABLE IF EXISTS `login_attempts`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table login_attempts
+		$this->db->query(<<<'EOT'
 CREATE TABLE `login_attempts` (
   `id` int(11) NOT NULL auto_increment,
   `ip_address` varchar(40) collate utf8_bin NOT NULL,
   `login` varchar(50) collate utf8_bin NOT NULL,
   `time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+EOT
+);
 
---
--- Table structure for table `mail`
---
-
-DROP TABLE IF EXISTS `mail`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table mail
+		$this->db->query(<<<'EOT'
 CREATE TABLE `mail` (
   `id` int(11) NOT NULL auto_increment,
   `subject` varchar(255) collate utf8_unicode_ci NOT NULL,
   `body` text collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `mail_participant_map`
---
-
-DROP TABLE IF EXISTS `mail_participant_map`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table mail_participant_map
+		$this->db->query(<<<'EOT'
 CREATE TABLE `mail_participant_map` (
   `id` int(11) NOT NULL auto_increment,
   `participantId` int(11) NOT NULL,
@@ -158,16 +116,12 @@ CREATE TABLE `mail_participant_map` (
   CONSTRAINT `mail_participant_map_ibfk_1` FOREIGN KEY (`participantId`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
   CONSTRAINT `mail_participant_map_ibfk_2` FOREIGN KEY (`participantId`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
   CONSTRAINT `mail_participant_map_ibfk_3` FOREIGN KEY (`mailId`) REFERENCES `mail` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1918 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=1918 DEFAULT CHARSET=latin1
+EOT
+);
 
---
--- Table structure for table `participants`
---
-
-DROP TABLE IF EXISTS `participants`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table participants
+		$this->db->query(<<<'EOT'
 CREATE TABLE `participants` (
   `id` int(11) NOT NULL auto_increment,
   `firstName` varchar(80) collate utf8_unicode_ci NOT NULL default '',
@@ -185,16 +139,12 @@ CREATE TABLE `participants` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `uuid` (`uuid`),
   UNIQUE KEY `uuid_2` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=11010 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=11010 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `poster_visitor_map`
---
-
-DROP TABLE IF EXISTS `poster_visitor_map`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table poster_visitor_map
+		$this->db->query(<<<'EOT'
 CREATE TABLE `poster_visitor_map` (
   `id` int(11) NOT NULL auto_increment,
   `presenterId` int(11) NOT NULL,
@@ -205,16 +155,12 @@ CREATE TABLE `poster_visitor_map` (
   KEY `visitorId` (`visitorId`),
   CONSTRAINT `seeMeAtMyPosterActivity_ibfk_1` FOREIGN KEY (`presenterId`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
   CONSTRAINT `seeMeAtMyPosterActivity_ibfk_2` FOREIGN KEY (`visitorId`) REFERENCES `participants` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=887 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=887 DEFAULT CHARSET=latin1
+EOT
+);
 
---
--- Table structure for table `silverback_answers`
---
-
-DROP TABLE IF EXISTS `silverback_answers`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table silverback_answers
+		$this->db->query(<<<'EOT'
 CREATE TABLE `silverback_answers` (
   `id` int(11) NOT NULL auto_increment,
   `day` enum('ASSIGN_ME','MONDAY','TUESDAY','WEDNESDAY','NOT_IN','ASSIGNED_MONDAY','ASSIGNED_TUESDAY') NOT NULL,
@@ -225,16 +171,12 @@ CREATE TABLE `silverback_answers` (
   PRIMARY KEY  (`id`),
   KEY `participantId` (`participantId`),
   CONSTRAINT `silverback_answers_ibfk_1` FOREIGN KEY (`participantId`) REFERENCES `participants` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1
+EOT
+);
 
---
--- Table structure for table `silverback_student_map`
---
-
-DROP TABLE IF EXISTS `silverback_student_map`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table silverback_student_map
+		$this->db->query(<<<'EOT'
 CREATE TABLE `silverback_student_map` (
   `id` int(11) NOT NULL auto_increment,
   `silverbackId` int(11) NOT NULL,
@@ -245,31 +187,23 @@ CREATE TABLE `silverback_student_map` (
   KEY `studentId` (`studentId`),
   CONSTRAINT `silverback_student_map_ibfk_1` FOREIGN KEY (`silverbackId`) REFERENCES `participants` (`id`) ON DELETE CASCADE,
   CONSTRAINT `silverback_student_map_ibfk_2` FOREIGN KEY (`studentId`) REFERENCES `participants` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=152 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=152 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `symposia`
---
-
-DROP TABLE IF EXISTS `symposia`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table symposia
+		$this->db->query(<<<'EOT'
 CREATE TABLE `symposia` (
   `id` int(11) NOT NULL,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `timetable`
---
-
-DROP TABLE IF EXISTS `timetable`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table timetable
+		$this->db->query(<<<'EOT'
 CREATE TABLE `timetable` (
   `id` int(11) NOT NULL auto_increment,
   `startTime` datetime NOT NULL,
@@ -278,16 +212,12 @@ CREATE TABLE `timetable` (
   `type` enum('WELCOME','PLENARY_TALK','GENERAL_SESSION','SYMPOSIA_SESSION','POSTER_PRESENTATION') collate utf8_unicode_ci NOT NULL,
   `info` text collate utf8_unicode_ci,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+EOT
+);
 
---
--- Table structure for table `user_autologin`
---
-
-DROP TABLE IF EXISTS `user_autologin`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table user_autologin
+		$this->db->query(<<<'EOT'
 CREATE TABLE `user_autologin` (
   `key_id` char(32) collate utf8_bin NOT NULL,
   `user_id` int(11) NOT NULL default '0',
@@ -295,32 +225,24 @@ CREATE TABLE `user_autologin` (
   `last_ip` varchar(40) collate utf8_bin NOT NULL,
   `last_login` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`key_id`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+EOT
+);
 
---
--- Table structure for table `user_profiles`
---
-
-DROP TABLE IF EXISTS `user_profiles`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table user_profiles
+		$this->db->query(<<<'EOT'
 CREATE TABLE `user_profiles` (
   `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `country` varchar(20) collate utf8_bin default NULL,
   `website` varchar(255) collate utf8_bin default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+EOT
+);
 
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+		## Create Table users
+		$this->db->query(<<<'EOT'
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(50) collate utf8_bin NOT NULL,
@@ -338,16 +260,48 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL default '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET character_set_client = @saved_cs_client;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+EOT
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+    $this->db->query('SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS');
+	 }
 
--- Dump completed on 2013-02-19 15:24:44
+	public function down()	{
+		### Drop table ci_sessions ##
+		$this->dbforge->drop_table("ci_sessions", TRUE);
+		### Drop table contributions ##
+		$this->dbforge->drop_table("contributions", TRUE);
+		### Drop table groups ##
+		$this->dbforge->drop_table("groups", TRUE);
+		### Drop table lhc_log ##
+		$this->dbforge->drop_table("lhc_log", TRUE);
+		### Drop table lhc_slides ##
+		$this->dbforge->drop_table("lhc_slides", TRUE);
+		### Drop table login_attempts ##
+		$this->dbforge->drop_table("login_attempts", TRUE);
+		### Drop table mail ##
+		$this->dbforge->drop_table("mail", TRUE);
+		### Drop table mail_participant_map ##
+		$this->dbforge->drop_table("mail_participant_map", TRUE);
+		### Drop table participants ##
+		$this->dbforge->drop_table("participants", TRUE);
+		### Drop table poster_visitor_map ##
+		$this->dbforge->drop_table("poster_visitor_map", TRUE);
+		### Drop table silverback_answers ##
+		$this->dbforge->drop_table("silverback_answers", TRUE);
+		### Drop table silverback_student_map ##
+		$this->dbforge->drop_table("silverback_student_map", TRUE);
+		### Drop table symposia ##
+		$this->dbforge->drop_table("symposia", TRUE);
+		### Drop table timetable ##
+		$this->dbforge->drop_table("timetable", TRUE);
+		### Drop table user_autologin ##
+		$this->dbforge->drop_table("user_autologin", TRUE);
+		### Drop table user_profiles ##
+		$this->dbforge->drop_table("user_profiles", TRUE);
+		### Drop table users ##
+		$this->dbforge->drop_table("users", TRUE);
+
+	}
+}
